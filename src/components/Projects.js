d682c2d677843b84fc5baac84a38fa3406b44860
ALMA_DEV914 <aungonalna58@gamil.com>
2024-08-08 00:00:54 -0700

    add projects descriptions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,37 +15,37 @@ const projects = [
     title: 'LOOKBOOK',
     description: "This website is designed to help individuals test their cognitive functions, specifically targeting Alzheimer's patients. It leverages modern web technologies such as React.js for an interactive user interface, Node.js with Express.js for robust backend support, and MongoDB with Mongoose for secure data management. User security is maintained with Bcrypt for password hashing and JWT (jsonwebtoken) for authentication.",
     image: projetc1,
-    link: '/projects/1'
+    link: '/'
   },
   {
     title: 'Ecommerce Site',
     description: 'This eCommerce website is powered by a robust technology stack, including MongoDB for database management, Redux for state management, and React for a dynamic frontend. The backend is built with Node.js and Express, ensuring efficient server-side operations. The app uses Redux to manage state, extract data, and dispatch actions, providing a seamless alternative to the Context API. Styling is handled with CSS, and payments are processed securely via PayPal.',
     image: projetc2,
-    link: '/projects/2'
+    link: '/'
   },
   {
     title: 'Blog Site',
     description: 'This blog website is a React application that leverages a REST API to fetch data from a third-party API source. It features a variety of cardio exercises, providing users with comprehensive information and resources for their fitness routines.',
     image: projetc3,
-    link: '/projects/3'
+    link: '/'
   },
   {
     title: 'Project Three',
     description: 'This application provides users with comprehensive details about movies to help them plan their next watch. Built using HTML, CSS, JavaScript, and integrated with third-party APIs, it displays movie ratings and plot summaries. The app utilizes Bootstrap and media queries for responsive design, and Git/GitHub for version control. It is designed to offer users an intuitive interface where they can easily view movie information, including ratings and plot summaries, to make informed decisions about their movie choices.',
     image: projetc4,
-    link: '/projects/3'
+    link: '/'
   },
   {
     title: 'Movie Review',
     description: 'This Weather Dashboard provides users with up-to-date weather information, including the current forecast and a 5-day weather outlook. Built with JavaScript, HTML, and CSS, and utilizing MongoDB, Node packages, and Bootstrap, this application fetches weather data using a third-party API. The Progressive Web App (PWA) features enhance user experience by offering offline capabilities and a responsive design. The dashboard is designed to help users easily access and understand weather conditions to plan their activities effectively.',
     image: projetc5,
-    link: '/projects/3'
+    link: '/'
   },
   {
     title: 'Quiz Puzzle',
     description: 'This Quiz Challenge application is built with HTML, JavaScript, and Web API. It leverages local storage to save and retrieve user data, allowing for a seamless quiz experience. The application runs directly in the browser and features dynamically updated HTML and CSS, which are controlled through JavaScript. This ensures an interactive and engaging user experience as users progress through the quiz.',
     image: projetc6,
-    link: '/projects/3'
+    link: '/'
   }
 ];
 
@@ -63,37 +63,31 @@ const Projects = () => {
         }}
       >
         My Projects
-      </Typography>
+      </Typography><br></br>
       <Grid container spacing={4}>
         {projects.map((project, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index} >
+          <Grid item xs={12} sm={6} md={4} key={index}>
             <motion.div
               initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: index * 0.2 }}
+              
             >
               <Card
                 sx={{
                   display: 'flex',
                   flexDirection: 'column',
-                  height: '100%',
-                  justifyContent: 'space-between'
+                  height: '600px', // Ensures card takes full height of its container
+                  maxHeight: '600px', // Optional: set a maximum height to prevent excessive card height
                 }}
               >
                 <CardMedia
                   component="img"
-                  height="140"
+                  height="100%"
                   image={project.image}
                   alt={project.title}
                 />
-                <CardContent
-                  sx={{
-                    flex: 1,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'space-between'
-                  }}
-                >
+                <CardContent sx={{ flex: '1 0 auto' }}>
                   <Typography gutterBottom variant="h5" component="div">
                     {project.title}
                   </Typography>
