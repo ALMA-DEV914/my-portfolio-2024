f4c35978e2523aa18d6ac884eb70d98865b57bec
ALMA_DEV914 <aungonalna58@gamil.com>
2024-08-07 21:49:50 -0700

    add expereince section and design

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,36 +5,48 @@ import { ReactComponent as MemeSVG } from "../assets/meme1.svg";
 const Contact = () => {
   return (
     <Container maxWidth="md" sx={{ py: 4, mt: "50px", mb: "50px" }}>
-      <Typography
-        variant="h1"
-        color="#4A4A4A"
-        sx={{
-          fontWeight: "bold",
-          fontSize: { xs: "2rem", sm: "3rem" }, // Responsive font size
-        }}
-        gutterBottom
-      >
-        Contact Me
-      </Typography>
       <Box
         sx={{
           display: "flex",
           flexDirection: { xs: "column", md: "row" },
-          alignItems: "center",
+          alignItems: { xs: "center", md: "flex-end" },
           gap: 2,
         }}
       >
+        {/* SVG Section */}
         <Box
           sx={{
             flex: 1,
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
+            
+            order: { xs: 1, md: 2 }, // Order the SVG to appear on the right on larger screens
           }}
         >
           <MemeSVG width="400" height="400" /> {/* Use your SVG component */}
         </Box>
-        <Box component="form" noValidate autoComplete="off" sx={{ flex: 2 }}>
+        
+        {/* Form Section */}
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+          sx={{ flex: 2, order: { xs: 2, md: 1,background: "#F9F9F9", padding:"15px" } }}
+        >
+          <Typography
+            variant="h1"
+            color="#4A4A4A"
+            sx={{
+              fontWeight: "bold",
+              fontSize: { xs: "2rem", sm: "3rem" }, // Responsive font size
+              mb: 2, // Margin bottom to create space below the header
+              textAlign: { xs: "center", md: "right" }, // Align text to the right on larger screens
+            }}
+            gutterBottom
+          >
+            Contact Me
+          </Typography>
           <TextField
             fullWidth
             label="Name"
@@ -55,10 +67,17 @@ const Contact = () => {
             multiline
             rows={4}
           />
-          <Button variant="contained" color="primary" sx={{ mt: 2, background: "#8D6E63",
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{
+              mt: 2,
+              background: "#8D6E63",
               "&:hover": {
                 background: "#6D4C41", // Hover background color
-              }, }}>
+              },
+            }}
+          >
             Send
           </Button>
         </Box>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,30 +4,51 @@ import {
   Typography,
   List,
   ListItem,
-  ListItemText,
   Divider,
+  Box,
 } from "@mui/material";
+import { styled } from "@mui/system";
 
 const experiences = [
   {
-    title: "Software Engineer at Company A",
+    title: "Freelance Web Developer at Freelancer.com",
     description:
-      "Worked on developing and maintaining web applications using React and Node.js.",
+      "Experienced frontend developer specializing in React, Next.js, and MUI. I build responsive, high-performance web applications and design intuitive user interfaces. Proven track record in delivering quality solutions that meet client needs and enhance user experiences.",
     date: "Jan 2022 - Present",
   },
   {
-    title: "Frontend Developer at Company B",
+    title: "Fullstack Web Developer at LOOKBOOK LLC",
     description:
-      "Designed and implemented user interfaces with a focus on performance and usability.",
-    date: "May 2020 - Dec 2021",
+      "Developed a full-stack web application using the MERN stack (MongoDB, Express.js, React, Node.js), delivering a dynamic and interactive user experience. Built responsive front-end components with React, designed RESTful APIs with Express.js, and managed data with MongoDB. Deployed the application on [Deployment Platform], incorporating CI/CD pipelines for streamlined updates. Successfully addressed challenges in scalability and security, resulting in a robust and user-friendly application.",
+    date: "April - July 2023",
   },
   {
-    title: "Intern at Company C",
+    title: "Frontend Developer at Queeery LLC",
     description:
-      "Assisted in building and testing web applications as part of the development team.",
-    date: "Jun 2019 - Aug 2019",
+      "Developed modern web applications using React, Next.js, and MUI. Created reusable UI components, implemented server-side rendering and static site generation, and integrated a consistent design system with MUI. Focused on performance optimization, responsive design, and collaboration with design and backend teams to deliver high-quality, user-centric interfaces.",
+    date: "Oct 2022 - Jan 2023",
+  },
+  {
+    title: "Bookkeeper - Southwest Employment Agency Singapore",
+    description:
+      "Responsible for recording and maintaining the company's daily financial transactions. Prepare reports for the company owner and trial balances. Introduced and implemented an online bookkeeping system that resulted in accurate and timely accounts records management. Balanced a particularly complicated balance sheet that had one entry missing and was in a state of limbo for three months. Reduced overhead costs by $4000 per month by suggesting the use of alternative energy during peak office hours. Stayed under budget for 15 months by ensuring that petty cash disbursement was minimized.",
+    date: "Jan 2013 - April 2014",
+  },
+  {
+    title: "City Treasurer - Dingalan, Aurora Philippines",
+    description:
+      "Oversaw all financial transactions and fundraising efforts going in or out of an organizing committee. Responsibilities in cash management, liquidity projections, treasury operations, bank relationship management, cash accounting, budget planning, financial reporting, record-keeping, and managing incoming and outgoing funds. Created the most effective, efficient, and accountable administration of all financial and tax collection activities for the city taxpayers. Continue the process of development and improvement of property records that are made easily accessible to other County departments, the general public, and professionals who have a need for the information contained within the treasurer's office.",
+    date: " April 2009 - July 2012",
   },
 ];
+
+const StyledListItem = styled(ListItem)(({ theme }) => ({
+  borderRadius: theme.shape.borderRadius,
+  padding: theme.spacing(2),
+  backgroundColor: theme.palette.background.paper,
+  boxShadow: theme.shadows[1],
+  marginBottom: theme.spacing(2),
+}));
 
 const Experience = () => {
   return (
@@ -36,7 +57,9 @@ const Experience = () => {
         variant="h1"
         sx={{
           fontWeight: "bold",
-          fontSize: { xs: "2rem", sm: "3rem" }, // Responsive font size
+          fontSize: { xs: "2rem", sm: "3rem" },
+          mb: 4,
+          textAlign: "center",
         }}
         color="#4A4A4A"
         gutterBottom
@@ -46,17 +69,19 @@ const Experience = () => {
       <List>
         {experiences.map((experience, index) => (
           <React.Fragment key={index}>
-            <ListItem>
-              <ListItemText
-                primary={experience.title}
-                secondary={experience.description}
-                primaryTypographyProps={{ variant: "h6" }}
-                secondaryTypographyProps={{ variant: "body2" }}
-              />
-              <Typography variant="body2" color="textSecondary">
-                {experience.date}
-              </Typography>
-            </ListItem>
+            <StyledListItem>
+              <Box>
+                <Typography variant="h5" component="div" sx={{ mb: 1 }}>
+                  {experience.title}
+                </Typography>
+                <Typography variant="body1" color="textSecondary">
+                  {experience.description}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" sx={{ mt: 1 }}>
+                  {experience.date}
+                </Typography>
+              </Box>
+            </StyledListItem>
             {index < experiences.length - 1 && <Divider />}
           </React.Fragment>
         ))}
